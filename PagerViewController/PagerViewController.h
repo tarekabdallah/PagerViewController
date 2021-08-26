de3097a0ecec9574a7691771bea1560822f34a2e
Tarek Abdallah <tarek.abdallah@360mea.com>
2021-08-26 21:53:33 +0400

    Initial Project Commit

---
+++ PagerViewController/PagerViewController.h
@@ -0,0 +1,18 @@
+//
+//  PagerViewController.h
+//  PagerViewController
+//
+//  Created by Tarek Abdallah on 26/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PagerViewController.
+FOUNDATION_EXPORT double PagerViewControllerVersionNumber;
+
+//! Project version string for PagerViewController.
+FOUNDATION_EXPORT const unsigned char PagerViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagerViewController/PublicHeader.h>
+
+
